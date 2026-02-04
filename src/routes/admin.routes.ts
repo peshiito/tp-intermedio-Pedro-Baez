8a2d34e9960e287409617163b25faf74f81ae65c
peshiito <pgbaez0@gmail.com>
2026-02-04 18:29:53 -0300

    prueba

--- src/routes/admin.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getUsuarios } from "../controllers/admin.controller";
-import { verifyToken, adminMiddleware } from "../middlewares/auth.middleware";
-
-const router = Router();
-
-router.get("/usuarios", verifyToken, adminMiddleware, getUsuarios);
-
-export default router;
